931fc03c2037e503e04d31c0d452ec3736ad3aab
Peter <pedrojuana08@gmail.com>
2025-11-27 14:40:59 +0800

    try

--- node_modules/.pnpm/node_modules/frontend/src/lib/axios.js
+++ node_modules/.pnpm/node_modules/frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://christianlifeparanaque.onrender.com/api',
+  baseURL: 'http://localhost:3000/api',
   withCredentials: true,
 });
