3e6a1ff23599ded0ece2d8039616d9fea82a4530
Peter <pedrojuana08@gmail.com>
2025-11-27 16:00:11 +0800

    try

--- node_modules/.pnpm/node_modules/frontend/vite.config.js
+++ node_modules/.pnpm/node_modules/frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Force React and ReactDOM to use the root workspace versions
+      react: path.resolve(__dirname, '../../node_modules/react'),
+      'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
+    },
+  },
 })
